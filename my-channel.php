70546e65b218c4fd6bf7dc441fc5893e3b6cf99a
John Doe <johndoe@email.com>
2024-07-12 20:54:00 +0600

    ...

--- my-channel.php
+++ my-channel.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Showing Channel <?php echo $_GET['channelID']?></h1>
+    <h1>Showing Channel <?php echo $_GET['channelID']?> his videos:</h1>
     
 </body>
 </html>
@@ -15,12 +15,11 @@
     $channelID=$_GET['channelID'];
     echo $channelID;
     include('database.php');
-    // $sql="SELECT * FROM channel WHERE ch_id = '$channelID'";
-    // $result = $conn->query($sql);
-    // if(mysqli_num_rows($result) > 0){ 
-    //    if($row = mysqli_fetch_assoc($result)){
-    //       echo $row['ch_name'];
-    //     } 
+
+    # who owns the channel-->
+    $sql = "SELECT * FROM user WHERE channel_id = '$channelID'"; #userX creates 1 channel only
+    $result = $conn->query($sql);
+
     // }
     #task: show all the video(profile) $channelID has uploaded 
 
@@ -32,19 +31,6 @@
         }
      }
      mysqli_close( $conn );
-     #can display channels beholder info as well using inner join in here||
+     
+     
 ?>
-
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-<script src="https://unpkg.com/feather-icons"></script>
-<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
-<script>
-    feather.replace();
-
-    var mySwiper = new Swiper('.swiper-container', {
-      // Optional parameters
-      slidesPerView: 'auto',
-      spaceBetween: 24,
-    });
-</script>
